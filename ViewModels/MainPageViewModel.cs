2fcf9f48b50a169d8724e7d6f25ca1eec8491b03
Jan Czerwiński <jan.ignacy.czerwinski@gmail.com>
2019-09-17 21:12:39 +0200

    Initial Commit

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ecomZadanie.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+       private ICommand _searchCommand;
+       public ICommand SearchCommand
+        {
+            get
+            {
+                return _searchCommand ?? (_searchCommand = new Command<string>((text) =>
+                {
+                    System.Diagnostics.Debug.WriteLine(text);
+                }));
+            }
+        }
+    }
+}
