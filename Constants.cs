f42d879542a0039bd56109f8d135d74313787f85
Jan Czerwiński <jan.ignacy.czerwinski@gmail.com>
2019-09-19 01:13:27 +0200

    Add downloading from api (in progress)

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecomZadanie
+{
+    public static class Constants
+    {
+        public static string UsersUrl = "https://internshiptaskuserslist.azurewebsites.net/api/users?code=gbgu4CbgdAlsS0xIVaNkckK4vTd0qIFNxaQYzIHLaqyomquJwuy/ig==";
+        public static string UserDetailUrl(int Id)  //this probably shouldnt be in constants
+        {
+            string baseUrl = "https://internshiptaskuserslist.azurewebsites.net/api/users/{0}?code=9XuCxWZqJavOAWHPcWD/97mMeJkK0mSVMA9A6MQ9n4R1B/6fpsxGqw==";
+            return string.Format(baseUrl, Id);
+        }
+    }
+}
