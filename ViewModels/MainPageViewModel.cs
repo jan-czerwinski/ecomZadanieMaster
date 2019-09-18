916e7c70cdc00c6c4208f59ee9115265315a5f77
Jan Czerwiński <jan.ignacy.czerwinski@gmail.com>
2019-09-18 21:11:41 +0200

    Add listView user display

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Prism.Commands;
+﻿using ecomZadanie.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
@@ -12,14 +14,28 @@ namespace ecomZadanie.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+
+            //Just a Users filler
+            for (int i = 0; i < 20; i++)
+            {
+                User user = new User
+                {
+                    FirstName = "Alberto",
+                    LastName = "Balsalm",
+                    Id = i
+                };
+                Users.Add(user);
+            }
         }
 
-       private ICommand _searchCommand;
-       public ICommand SearchCommand
+
+        private ICommand _searchCommand;
+        public ICommand SearchCommand
         {
             get
             {
