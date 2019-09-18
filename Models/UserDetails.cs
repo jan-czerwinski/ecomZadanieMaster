06fc5a62bcc67ad73cf2bd92e6057042231e6786
Jan Czerwiński <jan.ignacy.czerwinski@gmail.com>
2019-09-18 21:12:00 +0200

    Add user details model

---
+++ Models/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecomZadanie.Models
+{
+    public class UserDetails
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
